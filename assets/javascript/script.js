e90ec525029ff8d94081d078d1d8413d6e97896d
Amanda Morgan <AmandaC0022@gmail.com>
2021-08-14 20:27:07 -0500

    added comments to JS

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,13 @@
+// This controls the nav bar transition
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
   });
+
+  //TO DO: make screen transitions 
+
+  //TO DO: animate the projects page so that is it like an image carosal
+
+  //TO DO: get Contact Me section to work 
+
+  //TO DO: add downloadable resume feature 
