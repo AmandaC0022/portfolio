2f9a0ed267cb1335220eab18c84e30a7a8be0f49
Amanda Morgan <AmandaC0022@gmail.com>
2021-08-15 08:31:09 -0500

    created all global vars

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,38 @@
+//pages 
+var homePage = document.getElementById("home-page"); 
+var aboutMePage = document.getElementById("about-me-page"); 
+var projectsPage = document.getElementById("projects-page"); 
+var contactMePage = document.getElementById("contact-me-page"); 
+var resumePage = document.getElementById("resume-page"); 
+
+//nav bar links 
+var navAbout = document.getElementsByClassName("nav-about"); 
+var navProjects = document.getElementsByClassName("nav-projects"); 
+var navResume = document.getElementsByClassName("nav-resume"); 
+var navContact = document.getElementsByClassName("nav-contact"); 
+
+//buttons & links 
+var profileLink = document.getElementById("profile-link"); 
+var contactMeSubmit = document.getElementById("submit-btn"); 
+var projectLeftArrow = document.getElementsByClassName("left-arrow"); 
+var projectRightArrow = document.getElementsByClassName("right-arrow"); 
+
+//project cards 
+var brainBreaks = document.getElementById("brain-breaks"); 
+var weatherApp = document.getElementById("weather-app"); 
+var codeQuiz = document.getElementById("code-quiz"); 
+
 // This controls the nav bar transition
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
   });
 
+  function init() {
+
+  }
+
+  init(); 
   //TO DO: make screen transitions 
 
   //TO DO: animate the projects page so that is it like an image carosal
