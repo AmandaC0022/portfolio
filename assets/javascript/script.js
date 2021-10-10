6e4cbda0020e3f8506357cd93c22dc3eb913d0f5
Amanda Morgan <AmandaC0022@gmail.com>
2021-10-10 14:31:23 -0500

    changed navigation around page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -37,119 +37,68 @@ $(document).ready(function(){
   $('.scrollspy').scrollSpy();
 });
 
+//Collapsible Project Cards
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
 
 //hides all screens except for home 
-// function init() {
-//   aboutMePage.classList.add("hide"); 
-//   projectsPage.classList.add("hide"); 
-//   contactMePage.classList.add("hide"); 
-//   resumePage.classList.add("hide"); 
-// }
+function init() {
+  hideAllProjects(); 
+  brainBreaks.classList.remove("hide"); 
+}
 
-// init(); 
+init(); 
 
 
-// function hideAllPages() {
-//   homePage.classList.add("hide"); 
-//   aboutMePage.classList.add("hide"); 
-//   projectsPage.classList.add("hide"); 
-//   resumePage.classList.add("hide"); 
-//   contactMePage.classList.add("hide"); 
-// }
+//this hides all project cards 
+function hideAllProjects() { 
+  brainBreaks.classList.add("hide"); 
+  weatherApp.classList.add("hide"); 
+  codeQuiz.classList.add("hide"); 
+}
 
-//when user clicks home button, then they are taken to the home page
-// homeBtn.addEventListener("click", function(){
-//   homePage.classList.remove("hide"); 
-//   init(); 
-// }); 
+//removes active status from all project nav buttons
+function removeActive() {
+  document.getElementById("project-1").classList.remove("active-custom");  
+  document.getElementById("project-2").classList.remove("active-custom");  
+  document.getElementById("project-3").classList.remove("active-custom");  
+}
 
-//when user clicks on SEE MY FULL STORY, they are taken to the about me page
-// profileLink.addEventListener("click", function() {
-//   homePage.classList.add("hide"); 
-//   aboutMePage.classList.remove("hide"); 
-// }); 
+projectNav1.addEventListener("click", function() {
+  hideAllProjects(); 
+  brainBreaks.classList.remove("hide"); 
+  brainBreaks.classList.add("fadein"); 
+  removeActive(); 
+  document.getElementById("project-1").classList.add("active-custom"); 
 
-// //when user clicks on About in Nav bars then it takes them to the about me page 
-// navAbout[0].addEventListener("click", function() {
-//   hideAllPages(); 
-//   aboutMePage.classList.remove("hide"); 
-// }); 
-// navAbout[1].addEventListener("click", function() {
-//   hideAllPages(); 
-//   aboutMePage.classList.remove("hide"); 
-// });
+}); 
+projectNav2.addEventListener("click", function() {
+  hideAllProjects(); 
+  weatherApp.classList.remove("hide");
+  weatherApp.classList.add("fadein"); 
+  removeActive(); 
+  document.getElementById("project-2").classList.add("active-custom");  
 
-// //when user clicks on projects tab in nav, they are taken to the project page 
-// navProjects[0].addEventListener("click", function() {
-//   hideAllPages(); 
-//   hideAllProjects(); 
-//   projectsPage.classList.remove("hide"); 
-//   brainBreaks.classList.remove("hide"); 
-//   document.getElementById("project-1").classList.add("active-custom");  
-
-// })
-// navProjects[1].addEventListener("click", function() {
-//   hideAllPages();
-//   hideAllProjects();  
-//   projectsPage.classList.remove("hide"); 
-//   brainBreaks.classList.remove("hide"); 
-//   document.getElementById("project-1").classList.add("active-custom");  
-
-// })
-
-// //when user clicks on Contact me in the nav bar, then they are taken to the contact me page 
-// navContact[0].addEventListener("click", function() {
-//   hideAllPages(); 
-//   contactMePage.classList.remove("hide"); 
-// }); 
-// navContact[1].addEventListener("click", function() {
-//   hideAllPages(); 
-//   contactMePage.classList.remove("hide"); 
-// }); 
-
-// //when user clicks on resume link, they are taken to the pdf of the resume
-// navResume[0].addEventListener("click", function(){
-//   hideAllPages(); 
-//   resumePage.classList.remove("hide"); 
-// })
-// navResume[1].addEventListener("click", function(){
-//   hideAllPages(); 
-//   resumePage.classList.remove("hide"); 
-// })
-
-// //this hides all project cards 
-// function hideAllProjects() { 
-//   brainBreaks.classList.add("hide"); 
-//   weatherApp.classList.add("hide"); 
-//   codeQuiz.classList.add("hide"); 
-// }
-
-// //removes active status from all project nav buttons
-// function removeActive() {
-//   document.getElementById("project-1").classList.remove("active-custom");  
-//   document.getElementById("project-2").classList.remove("active-custom");  
-//   document.getElementById("project-3").classList.remove("active-custom");  
-// }
-
-// projectNav1.addEventListener("click", function() {
-//   hideAllProjects(); 
-//   brainBreaks.classList.remove("hide"); 
-//   removeActive(); 
-//   document.getElementById("project-1").classList.add("active-custom");  
-// }); 
-// projectNav2.addEventListener("click", function() {
-//   hideAllProjects(); 
-//   weatherApp.classList.remove("hide");
-//   removeActive(); 
-//   document.getElementById("project-2").classList.add("active-custom");  
-
-// }); 
-// projectNav3.addEventListener("click", function() {
-//   hideAllProjects(); 
-//   codeQuiz.classList.remove("hide"); 
-//   removeActive(); 
-//   document.getElementById("project-3").classList.add("active-custom");  
-// }); 
+}); 
+projectNav3.addEventListener("click", function() {
+  hideAllProjects(); 
+  codeQuiz.classList.remove("hide"); 
+  codeQuiz.classList.add("fadein"); 
+  removeActive(); 
+  document.getElementById("project-3").classList.add("active-custom");  
+}); 
 
 //TODO: get Contact Me section to work 
 
